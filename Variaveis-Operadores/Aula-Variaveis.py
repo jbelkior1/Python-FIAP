ca380fae08fe32aabc18ffd3041379dce7330813
jbelkior1 <joaobelkior29@gmail.com>
2026-03-19 20:57:27 -0300

    V.1 Aula 2 Variaveis e Operadores

---
+++ Variaveis-Operadores/Aula-Variaveis.py
@@ -0,0 +1,25 @@
+print ("Hello Word")
+
+print (8+3)
+print ("8+3")
+print ("7" + "9") #Concatenação de Strings
+
+# Variaveis
+
+nome= "João Belchior"
+
+idade = 19
+peso = 70
+altura = 1.80
+
+print(nome, idade, peso, altura)
+print(f"Esse cara é legal {nome}")
+
+#INPUT - SIMULAÇÃO DE USUÁRIOS NO CMD
+
+nome =input('Digite o seu nome: ')
+idade =int(input('Digite o seu idade: '))
+peso =float(input('Digite o seu peso: '))
+
+print(f"Essas são suas informações {nome, idade, peso}")
+print (idade + 1)
---
+++ Variaveis-Operadores/Desafios Python.py
@@ -0,0 +1,18 @@
+#Desafio 1
+
+nome = input("Digite o seu Nome aqui: ")
+print (f"Boas Vindas {nome}, THE FUTURE IS ROCK")
+
+#Desafio 2
+
+idade_dia = input("Digite seu dia de nascimento: ")
+idade_mes = input("Digite seu mes de nascimento: : ")
+idade_ano = input("Digite seu ano de nascimento: ")
+numero_soma1 = input("Digite um Número para testar: ")
+numero_soma2 = input("Digite outro Número para testar: ")
+
+print (f"Boas Vindas {nome}, THE FUTURE IS ROCK")
+print (f"Sua idade de nascimento é : {idade_dia}/{idade_mes}/{idade_ano}")
+print (f"Esse é o valor somado: {numero_soma1 + numero_soma2}")
+
+
